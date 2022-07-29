1b4cdf648d7ed07ad9bed78aed33cf5bf6095290
[Dogukan <dogukandoganhelsinki@gmail.com>
2022-07-29 21:18:09 +0300

    nav menu is done but need to fix redirection of the pages

--- Categories/Highest Rated/script.js
+++ Categories/Highest Rated/script.js
@@ -2,7 +2,7 @@ const API_KEY = "api_key=fcc651f2e319bf0891036e2b83989afc";
 const BASE_URL = "https://api.themoviedb.org/3";
 const API_URL =
   BASE_URL +
-  "/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22&" +
+  "/discover/movie/?certification_country=US&certification=R&sort_by=vote_average.desc&" +
   API_KEY;
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
 const container = document.querySelector(".container");
@@ -19,6 +19,7 @@ function getMovies(url) {
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
+      console.log(data.results);
       showMovies(data.results);
     });
 }
